46b1b8b7b57e54b17e9101851f32def6fb98a16e
lim950808 <lim080895@gmail.com>
2021-12-20 23:25:23 +0900

    22강

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-    Member save(Member member);
-    Optional<Member> findById(Long id);
-    Optional<Member> findByName(String name);
-    List<Member> findAll();
+    Member save(Member member); //회원 저장
+    Optional<Member> findById(Long id); //회원 id 찾아옴
+    Optional<Member> findByName(String name); //회원 name을 찾아옴
+    List<Member> findAll(); //저장된 모든 리스트를 반환함
 }
